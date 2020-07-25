883c4756ec84ba4b38f332c1500030cf8bd81080
arjun-sudo <laserarjun@gmail.com>
2020-07-26 00:59:41 +0545

    managing components

---
+++ src/components/MyInfo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// Using functional components 
+
+function MyInfo(){
+    return(
+    <div>
+      <h1>My name is Arjun Gautam.</h1>
+      <p>I am a passionate full stack developer .</p>
+      <h1>Expertise :</h1>
+      <ul>
+        <li>Spring Boot </li>
+        <li>React </li>
+        <li>MySql</li>
+      </ul>
+      </div>
+    )
+  }
+  export default MyInfo
--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import MyInfo from './components/MyInfo'
 
-// Using functional components 
-
-function MyInfo(){
-  return(
-  <div>
-    <h1>My name is Arjun Gautam.</h1>
-    <p>I am a passionate full stack developer .</p>
-    <h1>Expertise :</h1>
-    <ul>
-      <li>Spring Boot </li>
-      <li>React </li>
-      <li>MySql</li>
-    </ul>
-    </div>
-  )
-}
 
 ReactDOM.render(
   <MyInfo />
