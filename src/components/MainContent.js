7cd8e1f048319766062140fd9eafd178b31068f2
arjun-sudo <laserarjun@gmail.com>
2020-07-26 19:39:01 +0545

    parent and child components

---
+++ src/components/MainContent.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function MainContents()
+{
+    return(
+
+        <main>
+            This is the header .
+        </main>
+
+    )
+}
+export default MainContents
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import MyInfo from './components/MyInfo'
+
+import App from "./App"
+
+/* import MyInfo from './components/MyInfo'
 
 
 ReactDOM.render(
   <MyInfo />
   ,
   document.getElementById("root")
+) */
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+
 )
