89cfa3f3bb9e955c5b7b406c0ec5b259afae2237
arjun-sudo <laserarjun@gmail.com>
2020-07-27 07:05:12 +0545

    jsx to js

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,10 +2,26 @@ import React from "react"
 
 function MainContents()
 {
+    const date=new Date()
+    const hours=date.getHours()
+    let timeOfDay
+    if(hours<12)
+    {
+        timeOfDay="Good Morning "
+    }
+    else if(hours>=12 &&hours<17)
+    {
+        timeOfDay="Good AfterNoon "
+    }
+    else {
+        timeOfDay="Good Night "
+    }
     return(
 
         <main className="main">
-            This is the header .
+       
+       <h1>{timeOfDay} Budi .</h1>
+
         </main>
 
     )
