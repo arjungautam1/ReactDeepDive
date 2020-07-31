0b7b37c59cf1b357eadc1b27f27e9eac80e6d2bd
arjun-sudo <laserarjun@gmail.com>
2020-07-31 20:30:58 +0545

    mapping components to react

--- joke-punchline/src/App.js
+++ joke-punchline/src/App.js
@@ -1,51 +1,17 @@
 import React from "react"
+
 import Joke from "./components/Joke"
-import JokeData from "./components/JokeData"
 import jokeData from "./components/JokeData"
 
 function App() {
 
-  jokeData.map(fjoke=>{
-    return (
-      <Joke question={joke.question} 
-      
-    )
- 
-  })
+  const jokeComponents=jokeData.map(joke => <Joke key={joke.id} question={joke.question} answer={joke.answer}    />)
 
+  return(
+  <div>
+    {jokeComponents}
+  </div>
+)
 
-
-  /* Higher order methods  */
-//   const nums=[1,2,3,4,5]
-//   const doubled=nums.map(function(num)
-//   {
-//     return num*2
-//   })
-  
-// console.log(doubled)
-
-
-  return (
-    <div className="joke">
-
-
-      <Joke
-        joke={{ answer: "This doesn't have question." }}
-      />
-
-      <Joke
-
-        joke={{ question: "What is Joke ?",
-         answer: "You are Joke" }}
-      />
-
-      <Joke
-        joke={{ question: "How are you ?",
-         answer: "I am fine " }}
-      />
-
-    </div>
-  )
-
-}
+  }
 export default App
--- joke-punchline/src/components/Joke.js
+++ joke-punchline/src/components/Joke.js
@@ -3,11 +3,11 @@ function Joke(props)
 {
     console.log(props)
     return(
-        <div className="joke-qus">
+        <div >
 
        
 
-         <h1 style={{display:props.joke.question?"block": "none"}}>Question:{props.joke.question} </h1> 
+         <h1 style={{display:props.question?"block": "none"}}>Question:{props.question} </h1> 
 
         
       {/* <h1 style={{display: !props.joke.question && "none"}}> Question :{props.joke.question} </h1> */}
@@ -18,7 +18,7 @@ function Joke(props)
 
         {/* <h1 >Answer : {props.joke.answer}</h1> */}
 
-        <h1 style={{color: !props.joke.question && "#888888"}}>Answer: {props.joke.answer}</h1>
+        <h1 style={{color: !props.question && "#888888"}}>Answer: {props.answer}</h1>
 
         <hr />
         </div>
