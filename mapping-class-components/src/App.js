8fbdac7e16fbb19b38be9c697ce0b8dff7cbcb4d
arjun-sudo <laserarjun@gmail.com>
2020-08-01 10:57:58 +0545

    react class-components

---
+++ mapping-class-components/src/App.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+
+class App extends React.Component {
+
+  // own methods 
+  render() {
+
+    // methods implementation 
+    // inline styling 
+
+    return (
+      <div>
+        <h1>Code gos here </h1>
+
+        {/* aif we are using props in class based components  */}
+        {/* {this.props.whatever} */}
+
+      </div>
+    )
+  }
+}
+
+export default App
