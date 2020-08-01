6cfc2db8bc24e1eed57fa0af84e26edc0c85ba9b
arjun-sudo <laserarjun@gmail.com>
2020-08-01 16:02:04 +0545

    mapping components practice

--- mapping-class-components/src/index.js
+++ mapping-class-components/src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React,{Component} from "react"
+import ReactDOM from "react-dom"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends Component{
+  render()
+  {
+    return(
+      <div>
+        <Header username="Arjun" />
+        <Greeting />
+      </div>
+    )
+  }
+}
+class Header extends Component{
+  render()
+  {
+    return(
+    <header>
+      <p>Welcome,{this.props.username} !</p>
+    </header>
+    )
+  }
+}
+class Greeting extends Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  render()
+  {
+
+    const date=new Date()
+    const hours= date.getHours()
+    let timeOfDay
+
+    if(hours<12){
+      timeOfDay="Morning "
+    }
+    else if(hours>=12 && hours<17)
+    {
+      timeOfDay="Afternoon"
+    }
+    else{
+      timeOfDay="night"
+    }
+
+
+    return(
+      <h1>Good {timeOfDay} to you , sir or madam.</h1>
+    )
+
+
+  }
+}
+
+ReactDOM.render(<App />,
+  document.getElementById("root"))
