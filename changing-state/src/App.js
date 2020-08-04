c1a54c17808e87e056f1d24b3e789fed8ceb1dd9
arjun-sudo <laserarjun@gmail.com>
2020-08-04 21:04:53 +0545

    changing the state

---
+++ changing-state/src/App.js
@@ -0,0 +1,45 @@
+import React, {Component} from "react";
+
+class App extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    //If i want to change the count just to one
+    /*
+       handleClick(){
+           this.setState({count:1})
+       }
+    */
+
+    handleClick(){
+        this.setState(prevState =>{
+            return{
+                count:prevState.count+1
+            }
+        })
+    }
+
+
+
+    render() {
+        return (
+            <div>
+
+
+                <h1> {this.state.count}</h1>
+                <br/>
+                <button onClick={this.handleClick}>Click</button>
+
+            </div>
+        )
+    }
+
+}
+
+export default App
