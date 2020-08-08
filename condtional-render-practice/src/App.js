6d1ce4be9d777f39bc81bf602e87cccf4950778c
arjun-sudo <laserarjun@gmail.com>
2020-08-08 12:39:25 +0545

    conditional redering practice

---
+++ condtional-render-practice/src/App.js
@@ -0,0 +1,44 @@
+import React, {Component} from "react"
+
+/*
+Challenge:
+
+Given a stateless functional component:
+1. Follow the steps necessary to add state to it,
+2. Have state keep track of whether the user is logged in or not
+3. Add a button that logs the user in/out
+    a. extra challenge - make the button display "log in" if they're not logged in and "log out" if they are
+4. Display text that says "Logged in" if the user is logged in, or "Logged out" if they're not.
+*/
+
+export  default class App extends Component
+{
+    constructor()
+    {
+        super()
+        this.state = {
+            isLoggedIn: false
+        }
+        this.handleClick=this.handleClick.bind(this)
+    }
+    handleClick(){
+        this.setState(prevState =>{
+            return{
+                isLoggedIn:!prevState.isLoggedIn
+            }
+        })
+    }
+
+    render(){
+        let buttonText=this.state.isLoggedIn ? "Log out" :"Log in "
+        let displayText=this.state.isLoggedIn ? "Logged In " : " Logged Out "
+        return(
+
+            <div>
+                <button onClick={this.handleClick}> {buttonText}</button>
+                <h1>{displayText}</h1>
+            </div>
+        )
+    }
+
+}
